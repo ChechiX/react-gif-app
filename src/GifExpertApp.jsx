91fca5d8c893c716a0e67e01b6b83ece91cae6e4
Julio Anccasi <chechiofficial27@gmail.com>
2023-11-30 13:12:35 -0500

    First commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export const GifExpertApp = () => {
+
+  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory = () => {
+    // categories.push('Valorant');
+    setCategories([ 'Valorant', ...categories ]);
+    // setCategories( cat => [ ...cat, 'Valorant'] );
+  }
+
+  return (
+    <>
+      {/* Titulo */}
+      <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+
+      {/* Listado de Gif */}
+      <button onClick={ onAddCategory }>Agregar</button>
+      <ol>
+        { 
+          categories.map( category => {
+            return <li key={ category }>{ category }</li>
+          }) 
+        }
+        {/* <li>ABC</li> */}
+      </ol>
+        {/* Gif Item */}
+
+    </>
+  )
+}
